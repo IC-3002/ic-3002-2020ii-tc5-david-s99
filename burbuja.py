861870ba540abae607195d88dcf1bc0b4dd274f4
david-s99 <57419969+david-s99@users.noreply.github.com>
2020-10-07 23:42:24 -0600

    Update burbuja.py

--- burbuja.py
+++ burbuja.py
@@ -1,4 +1,17 @@
 
+#Ejercio tarea Corta 5
+def burbuja(A):
+    n = len(A)
+    for i in range(1, n):
+        for j in range(0, n - i):
+            if A[j] > A[j + 1]:
+                swap(A, j, j + 1)
+
+def swap(A, i, j):
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+
 def burbuja_optimizado(A):
     i=ordenado = verifica_ordenado(A)
     while i < len(A) and verifica_ordenado(A)==False:
@@ -28,3 +41,8 @@ def  verifica_ordenado(A):
         else:
             return False
     return True
+
+
+
+
+
